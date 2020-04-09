4464eb4961540ad82afaf071d00cce919244fb4e
上家吃瓜 <769505394@qq.com>
2020-04-09 15:22:59 +0800

    swagger-ui

--- springbootdemo/src/main/java/wen/tian/BootDemoApplication.java
+++ springbootdemo/src/main/java/wen/tian/BootDemoApplication.java
@@ -1,8 +1,10 @@
 package wen.tian;
 
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -11,17 +13,15 @@ import tk.mybatis.spring.annotation.MapperScan;
  * @author tianwenwen
  * @date 2019/7/25 21:13
  **/
+@EnableAsync
+@EnableSwagger2Doc
 @SpringBootApplication
 @MapperScan("wen.tian.mapper")
-@RestController
 public class BootDemoApplication {
 
     public static void main(String[] args){
         SpringApplication.run(BootDemoApplication.class);
     }
-    @RequestMapping
-    public String testDemo(){
-        return "HELLO SPRING BOOT";
-    }
+
 
 }
